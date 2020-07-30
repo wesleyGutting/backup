fd4d64ac3edc4839b03d0fd3576b4a0fcfbe0fc1
Wesley Gutting <wesleygutting@protonmail.com>
2020-07-29 21:37:35 -0400

    Added md5 hashing

--- Backup.py
+++ Backup.py
@@ -1,35 +1,56 @@
 import os
 import time
+import hashlib
+
+def log(logContent):
+    print(logContent)
+    logFile.write(logContent+"\n")
 
 def pathCheck(path):
     if(os.path.exists(path)):
         print(path + ": exists")
-        return 1
+        return True
     else:
         print(path + ': does not exist')
-        return 0
+        return False
 
+def calculateHash(input):
+    log(f"--Calculating md5 for: {input}")
+
+    BLOCK_SIZE = 65536
+    md5_hash = hashlib.md5()
+    with open(input, "rb") as f:
+        content = f.read(BLOCK_SIZE)
+        while len(content) > 0:
+            md5_hash.update(content)
+            content = f.read(BLOCK_SIZE)
+    log(f"----md5 Hash for: {input}: {md5_hash.hexdigest()}")
+    return md5_hash.hexdigest()
+    f.close()
+
+# path = 'C:\\Users\\wesle\\Desktop\\Colebrooke\\'
 path = 'C:\\Users\\wesle\\Desktop\\Software Dev\\Backup Program\\'
-# path = 'C:\\Users\\wesle\\Desktop\\Colebrooke'
 
 logTime = time.strftime("%y-%m-%dT%H%M%S", time.gmtime())
 
-if(pathCheck(path+".logs")==True):
-    os.chdir(path+".logs")
+if(pathCheck(f"{path}.logs")==True):
+    os.chdir(f"{path}.logs")
 else:
     print(f'Creating {path}.logs')
-    os.mkdir(path+".logs")
-    os.chdir(path+".logs")
+    os.mkdir(f"{path}.logs")
+    os.chdir(f"{path}.logs")
+
 print(f"Current Working Directory: {os.getcwd()}")
-logFile = open(logTime+"_LOG.txt", "w+")
+logFile = open(logTime+"_log.txt", "w+")
 os.chdir(path)
 
-for dirPath, dirNames, files in os.walk(path, topdown=True):
+fileList = {}
+for dirPath, dirNames, fileNames in os.walk(path, topdown=True):
     dirNames[:] = [d for d in dirNames if not d.startswith('.')]
-    print(f'Found Directory: {dirPath}')
-    logFile.write(f'Found Directory: {dirPath}\n')
-    for fileName in files:
-        print(fileName)
-        logFile.write(fileName+"\n")\
+    log(f'Found Directory: {dirPath}')
+    for file in fileNames:
+        log(f'Found File: {file}')
+        fileList[file] = calculateHash(file)
 
+print(fileList)
 logFile.close()
