af8c088c9aad0fb0fce776baf64c206c2906b5cb
Wesley Gutting <wesleygutting@protonmail.com>
2020-07-30 14:34:33 -0400

    Added iteration over file hashes to create a dictionary of files that need to be copied to the backup location

--- Backup.py
+++ Backup.py
@@ -2,55 +2,65 @@ import os
 import time
 import hashlib
 
-def log(logContent):
-    print(logContent)
-    logFile.write(logContent+"\n")
+BLOCK_SIZE = 65536
+WORKING_PATH = 'C:\\Users\\wesle\\Desktop\\Colebrooke\\'
+BACKUP_PATH = 'C:\\Users\\wesle\\Desktop\\Colebrooke_2\\'
+TEST_PATH = 'C:\\Users\\wesle\\Desktop\\Software Dev\\Backup Program\\'
 
-def pathCheck(path):
-    if(os.path.exists(path)):
-        print(path + ": exists")
-        return True
-    else:
-        print(path + ': does not exist')
-        return False
+#-------------------------------------------------------------------------------
 
 def calculateHash(input):
-    log(f"--Calculating md5 for: {input}")
-
-    BLOCK_SIZE = 65536
     md5_hash = hashlib.md5()
     with open(input, "rb") as f:
         content = f.read(BLOCK_SIZE)
         while len(content) > 0:
             md5_hash.update(content)
             content = f.read(BLOCK_SIZE)
-    log(f"----md5 Hash for: {input}: {md5_hash.hexdigest()}")
     return md5_hash.hexdigest()
     f.close()
 
-# path = 'C:\\Users\\wesle\\Desktop\\Colebrooke\\'
-path = 'C:\\Users\\wesle\\Desktop\\Software Dev\\Backup Program\\'
+#-------------------------------------------------------------------------------
 
-logTime = time.strftime("%y-%m-%dT%H%M%S", time.gmtime())
+def generateDict(path):
+    hashList = {}
+    for dirPath, dirNames, fileNames in os.walk(path, topdown=True):
+        dirNames[:] = [d for d in dirNames if not d.startswith('.')]
+        print(f"Found Directory: {dirPath}")
+        for file in fileNames:
+            print(f"Generating hash for: {file}")
+            hash = calculateHash(os.path.join(dirPath, file))
+            print(f"--Hash: {hash}")
+            hashList[hash] = os.path.join(dirPath, file)
+    return hashList
 
-if(pathCheck(f"{path}.logs")==True):
-    os.chdir(f"{path}.logs")
-else:
-    print(f'Creating {path}.logs')
-    os.mkdir(f"{path}.logs")
-    os.chdir(f"{path}.logs")
+#-------------------------------------------------------------------------------
 
-print(f"Current Working Directory: {os.getcwd()}")
-logFile = open(logTime+"_log.txt", "w+")
-os.chdir(path)
+def compareDicts(dict1, dict2):
+    dict = {}
+    update = dict1.keys() - dict2.keys()
+    for key in update:
+        dict[key] = dict1[key]
+    return dict
+#-------------------------------------------------------------------------------
 
-fileList = {}
-for dirPath, dirNames, fileNames in os.walk(path, topdown=True):
-    dirNames[:] = [d for d in dirNames if not d.startswith('.')]
-    log(f'Found Directory: {dirPath}')
-    for file in fileNames:
-        log(f'Found File: {file}')
-        fileList[file] = calculateHash(file)
+backupDict = {}
+workingDict = {}
 
-print(fileList)
-logFile.close()
+print("Generating file hashes")
+backupDict = generateDict(BACKUP_PATH)
+workingDict = generateDict(TEST_PATH)
+
+print("---------")
+
+print("Backup Hashes:")
+for key, value in backupDict.items():
+    print(key, value)
+print("Working Hashes:")
+for key, value in workingDict.items():
+    print(key, value)
+
+print("---------")
+
+updateDict = compareDicts(workingDict, backupDict)
+for key in updateDict:
+    print(updateDict[key])
